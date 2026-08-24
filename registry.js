c68cf5d563b9a0af6c9dd33dbe85e2f4d62324b2
hephaestic <hephaestic@gmail.com>
2026-08-24 00:19:17 -0700

    update registry.js

---
+++ registry.js
@@ -0,0 +1,31 @@
+/*
+  RECIPE REGISTRY
+
+  Whenever you add recipes/<id>.js, add its id below.
+
+  Example:
+  1. Create: recipes/ginger-fried-rice.js
+  2. Add: "ginger-fried-rice" to RECIPE_IDS
+*/
+
+window.RECIPE_IDS = [
+  "quinoa-lentils-greens",
+  "chicken-red-dates-ginger",
+  "miso-burdock",
+  "seaweed-soup",
+  "hearty-sausage-stew",
+  "oxtail-stew",
+  "pickled-congee-tea-eggs",
+  "ginger-fried-rice",
+
+  "unohana",
+  "ganmodoki",
+  "korokke",
+  "kongbiji",
+  "bijijeon",
+  "doufuzha",
+  "zhadoufu",
+  "bolognese",
+  "gyoza",
+  "friedrice",
+];
